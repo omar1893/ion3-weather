25db0c745d04fc466c39215713fc3251dbcd92f6
Omar López <olopez@momentumlab.co>
2017-09-19 09:44:23 -0430

    first commit

---
+++ src/providers/weather/weather.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class WeatherProvider {
+  apiKey = 'd2d229cca26f841d';
+  url:string;
+  constructor(public http: Http) {
+    console.log('Hello WeatherProvider Provider');
+    this.url = "http://api.wunderground.com/api/"+this.apiKey+"/conditions/q"
+  }
+
+  getWeather(city,state){
+    return this.http.get(this.url+'/'+state+'/'+city+'.json')
+    .map(res => res.json());
+  }
+}
